ee1a48385072b2f8536a2152789bd5d65c9d07b2
Enzo <enzo.lpez.pstor@gmail.com>
2025-08-12 15:08:37 -0400

    feat: actualizacion de vistas para home, register y login

--- platform_ia/website/views.py
+++ platform_ia/website/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-  return render(request, 'home.html', {})
+  return render(request, 'home.html', {'dropdown_val': 0})
 
 def login(request):
-  return render(request, 'auth/login.html', {})
+  return render(request, 'auth/login.html', {'is_auth': True})
+
+def register(request):
+  return render(request, 'auth/register.html', {'is_auth': True})
