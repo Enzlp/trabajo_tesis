d77eb4408d6b25556942809cfe408f305abcb542
Enzo <enzo.lpez.pstor@gmail.com>
2025-09-25 13:17:39 -0300

    feat: avance paginas front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    </>
+    <div className='flex flex-col'>
+      <Navbar />
+      <Router >
+        <Routes>
+          
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar(){
+  return(
+    <div className="flex justify-start">
+      <div>
+        <h1 className="font-semibold">Collaborator</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
