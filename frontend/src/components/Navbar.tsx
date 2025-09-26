0ff8d23b69bfa3c8e1b49d719f96b4726629d2ac
Enzo <enzo.lpez.pstor@gmail.com>
2025-09-25 21:45:58 -0300

    feat: componente navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-function Navbar(){
-  return(
-    <div className="flex justify-start">
-      <div>
-        <h1 className="font-semibold">Collaborator</h1>
+import logo from '../assets/platform_logo_v2.svg';
+
+function Navbar() {
+  return (
+      <div className="w-full flex items-center px-2  space-x-2 items-center justify-center">
+        <img src={logo} className='w-8 h-8'></img>
+        <h1 className="font-bold text-[#00d1b2] text-2xl">Colaborador IA</h1>
       </div>
-    </div>
-  )
+  );
 }
 
 export default Navbar;
